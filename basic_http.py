15081e91c95f2b990abaaa6424c6107510f02894
AZR88 <azrielamaldany@gmail.com>
2024-06-08 18:05:33 +0700

    assertion

--- basic_http.py
+++ basic_http.py
@@ -1,9 +1,12 @@
 from locust import task, HttpUser, constant
+from assertpy import assert_that
 
-class regres(HttpUser):
+
+class Regres(HttpUser):
     host = "https://reqres.in/"
-    self_time = constant(2)
+    
 
     @task
     def test_get(self):
-        self.client.get("api/users?page=2")
+        response = self.client.get("api/users?page=2")
+        assert_that(response.status_code).is_equal_to(200)
